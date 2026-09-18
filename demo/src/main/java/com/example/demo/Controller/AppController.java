84a2aaa8c18801ab50151cd1ee696144c9ded950
Harijith <harijithm2@gmail.com>
2026-09-18 09:01:03 +0530

    day 2

--- demo/src/main/java/com/example/demo/Controller/AppController.java
+++ demo/src/main/java/com/example/demo/Controller/AppController.java
@@ -1,4 +1,31 @@
 package com.example.demo.Controller;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.Service.AppService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.example.demo.model.AppModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
 public class AppController {
+    @Autowired
+    AppService service;
+    @PostMapping("/create")
+    public AppModel create(@RequestBody AppModel take1){
+       return service.create(take1);
+    }
+    @GetMapping("/getAll")
+    public List<AppModel> getAll() {
+        return service.getAll();
+    }
+    
+    
     
 }
--- demo/src/main/java/com/example/demo/Service/AppService.java
+++ demo/src/main/java/com/example/demo/Service/AppService.java
@@ -1,4 +1,22 @@
 package com.example.demo.Service;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.Repo.AppRepo;
+import com.example.demo.model.AppModel;
+
+@Service
 public class AppService {
-    
+    @Autowired 
+    AppRepo repository;
+    public AppModel create(AppModel take1){
+        return repository.save(take1);
+    }
+    //
+    public List<AppModel> getAll(){
+        return repository.findAll();
+    }
 }
